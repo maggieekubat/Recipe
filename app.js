560e4876ba426950811002790bb7d215affd5a8a
Maggie Kubat <maggieekubat@gmail.com>
2024-03-07 19:04:14 +0100

    middlewares/logger.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import { logger } from './middleware/logger.js'
 
 const app = express()
 const PORT = 3000
+app.use(logger)
 
 app.get('/', (request, response) => {
     response.send('Welcome to my 🍪 Cookieshop!')
