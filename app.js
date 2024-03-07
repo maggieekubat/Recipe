de3f54e57d9da5fcd9902f915987d26a9e79bfcc
Maggie Kubat <maggieekubat@gmail.com>
2024-03-07 18:58:15 +0100

    working on backend from the resources

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 import express from 'express'
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (request, response) => {
+    response.send('Welcome to my 🍪 Cookieshop!')
+})
+
+app.get('/contact', (request, response) => {
+    response.send('Reach out to us if you have any questions.')
+})
+
+app.get('/recipe/:slug', (request, response) => {
+    const recipeId = request.params.slug
+
+    response.send(`A recipe with the name '${recipeId}' could not be found`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Started server on port ${PORT}`)
+})
