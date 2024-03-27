4f5c9bdd9cfc67c49dac1fcfa0b5e02bf2de5d4e
Maggie Kubat <maggieekubat@gmail.com>
2024-03-27 12:41:41 +0100

    searching for recipes with different formatting works now

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
 import express from 'express'
 import { logger } from './middlewares/logger.js'
 
+//import { json } from './data.json'
+//const data = json
+//const data = require('./data.json')
+
+const recipes = {
+    mushroom_rice : [
+        "mushroom rice",
+        "mushroomrice",
+        "mushroom-rice"
+    ],
+    gnocchi_bake : [
+        "gnocchi bake", 
+        "gnocchibake",
+        "gnocchi-bake"
+    ]
+
+}
+
 const app = express()
 app.set('view engine', 'ejs')
 const PORT = 3000
 app.use(logger)
 app.use('/recipe', express.static('public/recipe'))
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (request, response) => {
     response.render('index')
 })
 
-app.get('/contact', (request, response) => {
-    response.send('Reach out to us if you have any questions.')
+app.get('/product', (request, response) => {
+    response.send(`The product name is ${data.title}`)
 })
+
+app.post('/contact', (request, response) => {
+    console.log('contact form submission', request.body)
+    response.send('Thanks for your message. We will be in touch soon')
+})
+
 
 app.get('/recipe/:slug', (request, response) => {
     const recipeId = request.params.slug
+    for (const [key, value] of Object.entries(recipes)){
+        for (const slugName of value) {
+            if (recipeId === slugName && key === "mushroom_rice") {
+                response.render("mushroom_rice")
+            } else if (recipeId === slugName && key === "gnocchi_bake"){
+                response.render("gnocchi_bake")
+            }
+        }
 
-    response.send(`A recipe with the name '${recipeId}' could not be found`)
+    }
+
+    //response.send(`A recipe with the name '${recipeId}' could not be found`)
 })
 
 app.listen(PORT, () => {
---
+++ test.js
@@ -0,0 +1,94 @@
+
+// {
+// const http = require('node:http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+
+//   console.log(req.url)
+
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+//   if (req.url == '/about') {
+//     res.end('About Page');
+//   }
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+// }
+
+//{'mushroom_rice': '["mushroom rice", "mushroomrice", "mushroom-rice", ]'};
+//{'gnocchi_bake': '["gnocchi bake", "gnocchibake", "gnocchi-bake", ]'};
+
+//const reci = [
+//    {'mushroom_rice': 'mushroom rice', 'mushroomrice', 'mushroom-rice',}
+//    {'gnocchi_bake': 'gnocchi bake', 'gnocchibake', 'gnocchi-bake',}
+//]
+
+
+
+// Useful Functions
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+
+const recipes = {
+    mushroom_rice: [
+        "mushroom rice",
+        "mushroomrice",
+        "mushroom-rice"
+    ],
+    gnocci_bake : [
+        "gnocchi bake", 
+        "gnocchibake",
+        "gnocchi-bake"
+    ]
+
+}
+
+// const recipes = {
+//     "mushroom_rice" : ["mushroom rice", "mushroomrice", "mushroom-rice"],
+//     "gnocci_bake" : ["gnocchi bake", "gnocchibake", "gnocchi-bake"]
+// }
+
+app.get('/recipe/:slug', (request, response) => {
+    const recipeId = request.params.slug
+    for (const [key, value] of recipes.entries()){
+        
+    }
+    if (a) {
+
+    }
+    else {
+        response.send(`A recipe with the name '${recipeId}' could not be found`)
+    }
+    
+})
+
+
+
+
+
+
+getKeyByValue(recipes, mushroom_rice)
+  
+for (let [key, value] of Object.entries(recipes)){
+    if ('$[value] == ""')
+        if (recipeId == recipes[mushroom_rice]) {
+            response.render('mushroom_rice.html')
+        }
+    }
+
+
+
+// Make a List here with recipes so that a user can
+// look up mushroom rice without typing the .html
+
+// Also maybe do another recipe so that its not just a list of one recipe
+
+
+recipes[mushroom_rice.html] = ["mushroom rice", "mushroomrice", "mushroom-rice", ]
+recipes[gnocchi_bake.html] = ["gnocchi bake", "gnocchibake", "gnocchi-bake", ]
